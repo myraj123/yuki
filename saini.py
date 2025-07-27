431a244ee91413c9e80c399cc3b284ea9f199247
rajbtp <130836047+myraj123@users.noreply.github.com>
2025-07-27 17:34:59 +0530

    Update saini.py

--- saini.py
+++ saini.py
@@ -33,7 +33,7 @@ def get_mps_and_keys(api_url):
     response = requests.get(api_url)
     response_json = response.json()
     mpd = response_json.get('MPD')
-    keys = response_json.get('keys')
+    keys = response_json.get('KEYS')
     return mpd, keys
    
 def exec(cmd):
